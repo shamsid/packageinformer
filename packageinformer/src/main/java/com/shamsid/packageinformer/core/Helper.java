08278d8c20772fb50cfd313e7e491f0c8ce66e60
Shamsher Ahmed <shamshersiddiqui@hotmail.com>
2017-02-15 02:27:28 +0530

    added git hash key finder

--- packageinformer/src/main/java/com/shamsid/packageinformer/core/Helper.java
+++ packageinformer/src/main/java/com/shamsid/packageinformer/core/Helper.java
@@ -2,6 +2,13 @@ package com.shamsid.packageinformer.core;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by shamsheR on 14/02/17.
@@ -18,6 +25,29 @@ public class Helper {
       }
     }
     return false;
+  }
+
+  public String generateHashkey(Context context){
+    String hash_key = "";
+    try {
+
+      PackageInfo info = context.getPackageManager().getPackageInfo(
+          "com.shamsid.loginmanager",
+          PackageManager.GET_SIGNATURES);
+      for (Signature signature : info.signatures) {
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        md.update(signature.toByteArray());
+
+        hash_key= ""+ Base64.encodeToString(md.digest(),Base64.NO_WRAP);
+      }
+    } catch (PackageManager.NameNotFoundException e) {
+      Log.d("NameNotFoundException", e.getMessage(), e);
+    } catch (NoSuchAlgorithmException e) {
+      Log.d("NoSuchAlException", e.getMessage(), e);
+    }
+    finally {
+      return hash_key;
+    }
   }
 
 
